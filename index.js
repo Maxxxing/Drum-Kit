5422db627e50f82129caf8ed25f729a62dfe6e68
Maxxxing <mingma0626@gmail.com>
2023-01-06 14:06:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
         
-        makeSound(uttonInnerHTML);
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 document.addEventListener("keydown", function(event) {
     makeSound(event.key)
+    buttonAnimation(event.key)
 });
 
 function makeSound(key){
@@ -44,4 +46,13 @@ function makeSound(key){
             break;
         default: console.log(buttonInnerHTML);
     }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout( function() {
+        activeButton.classList.remove("pressed");
+    }, 100)
 }
